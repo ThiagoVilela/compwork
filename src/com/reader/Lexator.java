aa51ad8e0b20a18f1a44ac76ad66615753a966f0
Thiago Vilela <mg.thiago@gmail.com>
2018-07-11 18:31:44 -0300

    Commit #03 - Preenchendo o Hashtable com o Enum

--- src/com/reader/Lexator.java
+++ src/com/reader/Lexator.java
@@ -22,5 +22,38 @@ public class Lexator {
 		this.column = 0;
 		this.symboltable = new ArrayList<String>();
 		this.liner = new LineNumberReader(new FileReader(filename));
+		
+		reservedWords = new Hashtable<String,TokenSigns>();
+		for (TokenSigns myenum : TokenSigns.values()) {
+			reservedWords.put(myenum.toString(), myenum);
+		}
+		
+		/*
+		reservedWords.put(TokenSigns.ABSTRACT.toString(), TokenSigns.ABSTRACT);
+        reservedWords.put(TokenSigns.BOOLEAN.toString(), TokenSigns.BOOLEAN);
+        reservedWords.put(TokenSigns.CHAR.toString(), TokenSigns.CHAR);
+        reservedWords.put(TokenSigns.CLASS.toString(), TokenSigns.CLASS);
+        reservedWords.put(TokenSigns.ELSE.toString(), TokenSigns.ELSE);
+        reservedWords.put(TokenSigns.EXTENDS.toString(), TokenSigns.EXTENDS);
+        reservedWords.put(TokenSigns.FALSE.toString(), TokenSigns.FALSE);
+        reservedWords.put(TokenSigns.IF.toString(), TokenSigns.IF);
+        reservedWords.put(TokenSigns.IMPORT.toString(), TokenSigns.IMPORT);
+        reservedWords.put(TokenSigns.INSTANCEOF.toString(), TokenSigns.INSTANCEOF);
+        reservedWords.put(TokenSigns.INT.toString(), TokenSigns.INT);
+        reservedWords.put(TokenSigns.NEW.toString(), TokenSigns.NEW);
+        reservedWords.put(TokenSigns.NULL.toString(), TokenSigns.NULL);
+        reservedWords.put(TokenSigns.PACKAGE.toString(), TokenSigns.PACKAGE);
+        reservedWords.put(TokenSigns.PRIVATE.toString(), TokenSigns.PRIVATE);
+        reservedWords.put(TokenSigns.PROTECTED.toString(), TokenSigns.PROTECTED);
+        reservedWords.put(TokenSigns.PUBLIC.toString(), TokenSigns.PUBLIC);
+        reservedWords.put(TokenSigns.RETURN.toString(), TokenSigns.RETURN);
+        reservedWords.put(TokenSigns.STATIC.toString(), TokenSigns.STATIC);
+        reservedWords.put(TokenSigns.SUPER.toString(), TokenSigns.SUPER);
+        reservedWords.put(TokenSigns.THIS.toString(), TokenSigns.THIS);
+        reservedWords.put(TokenSigns.TRUE.toString(), TokenSigns.TRUE);
+        reservedWords.put(TokenSigns.VOID.toString(), TokenSigns.VOID);
+        reservedWords.put(TokenSigns.WHILE.toString(), TokenSigns.WHILE);
+		*/
+		
 	}
 }
